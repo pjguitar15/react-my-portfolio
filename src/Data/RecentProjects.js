4102fa4137f6e24278c6d49658f07e434d9253e9
Phil <pjguitar15@gmail.com>
2021-03-03 11:18:43 +0800

    added new recent project card

--- src/Data/RecentProjects.js
+++ src/Data/RecentProjects.js
@@ -1,6 +1,7 @@
 import guitarapp from '../Assets/guitarapp.JPG'
 import expenseapp from '../Assets/expense-calculator-app.JPG'
 import todolist from '../Assets/todolist.JPG'
+import cookie from '../Assets/cookie.JPG'
 import ColorPalleteApp from '../Assets/color-pallete-app.JPG'
 import RandomNumberApp from '../Assets/random-number-app.JPG'
 
@@ -16,24 +17,16 @@ const RecentProjectsData = [
   },
   {
     id: 2,
-    title: 'Todo-list App',
-    text: 'Todo list has always been known as the first step in learning React. This is one of my early React projects which taught me all about basics, its syntax, and understanding the concept of States and Lifecycle.',
-    image: todolist,
-    tags: ['React', 'CSS'],
-    link: '#',
-    github: 'https://github.com/pjguitar15/react-todolist-app'
+    title: 'Social Media App',
+    text: 'This is my current project which is a CRUD app that Im still working on. To make it a dynamic website, I am working with MongoDB for my storing datas from user. My goal is to also be able to create my own full stack website',
+    image: cookie,
+    tags: ['Mern Stack'],
+    link: 'https://friendly-sinoussi-7c07a3.netlify.app/',
+    github: 'https://github.com/pjguitar15/react-cookie'
   },
+
   {
     id: 3,
-    title: 'Expense Tracker',
-    text: 'One of my early simple projects I created using Vanilla Javascript. It is a cash tracker that allows user manage its money using cash in, cashout system and tracks expenses.',
-    image: expenseapp,
-    tags: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
-    link: 'https://philcobexpensecalculator.netlify.app/',
-    github: 'https://github.com/pjguitar15/expense-calculator'
-  },
-  {
-    id: 4,
     title: 'Color Pallete App',
     text: 'One of my early React projects. Using useState, I was able to create the color change functionality. I used an npm package to handle the copy text function',
     image: ColorPalleteApp,
@@ -42,14 +35,32 @@ const RecentProjectsData = [
     github: 'https://github.com/pjguitar15/react-color-pallete'
   },
   {
-    id: 5,
+    id: 4,
     title: 'Random Number Generator',
     text: 'Using built in Javascript method, I was able to generate random numbers based on the user input. It is a very simple project but it can also be useful to some.',
     image: RandomNumberApp,
     tags: ['React', 'CSS'],
     link: 'https://boring-booth-4f6c67.netlify.app/',
     github: 'https://github.com/pjguitar15/react-random-number-generator'
-  }
+  },
+  {
+    id: 5,
+    title: 'Todo-list App',
+    text: 'Todo list has always been known as the first step in learning React. This is one of my early React projects which taught me all about basics, its syntax, and understanding the concept of States and Lifecycle.',
+    image: todolist,
+    tags: ['React', 'CSS'],
+    link: '#',
+    github: 'https://github.com/pjguitar15/react-todolist-app'
+  },
+  {
+    id: 6,
+    title: 'Expense Tracker',
+    text: 'One of my early simple projects I created using Vanilla Javascript. It is a cash tracker that allows user manage its money using cash in, cashout system and tracks expenses.',
+    image: expenseapp,
+    tags: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
+    link: 'https://philcobexpensecalculator.netlify.app/',
+    github: 'https://github.com/pjguitar15/expense-calculator'
+  },
 ]
 
 export default RecentProjectsData
