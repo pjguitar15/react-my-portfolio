5327611876c07a6dd0be5a79825b070549a7124d
Phil <pjguitar15@gmail.com>
2021-04-21 12:47:19 +0800

    removed some projects

--- src/Data/RecentProjects.js
+++ src/Data/RecentProjects.js
@@ -42,15 +42,15 @@ const RecentProjectsData = [
     link: 'https://cranky-noyce-d86d5b.netlify.app/',
     github: 'https://github.com/pjguitar15/react-shophub'
   },
-  {
-    id: 19,
-    title: 'Ladies Apparel E-Commerce (Libre Baskerville)',
-    text: 'Based from my own design, I used Adobe Illustrator to visualize the design and coded it using React.',
-    image: ladiesapparel,
-    tags: ['React', 'Firebase', 'Paypal API'],
-    link: 'https://tender-einstein-7800e2.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-ladies-apparel'
-  },
+  // {
+  //   id: 19,
+  //   title: 'Ladies Apparel E-Commerce (Libre Baskerville)',
+  //   text: 'Based from my own design, I used Adobe Illustrator to visualize the design and coded it using React.',
+  //   image: ladiesapparel,
+  //   tags: ['React', 'Firebase', 'Paypal API'],
+  //   link: 'https://tender-einstein-7800e2.netlify.app/',
+  //   github: 'https://github.com/pjguitar15/react-ladies-apparel'
+  // },
   {
     id: 8,
     title: 'Movie App',
@@ -96,62 +96,25 @@ const RecentProjectsData = [
     link: 'https://philcobguitarshop.netlify.app/',
     github: 'https://github.com/pjguitar15/react-guitar-ecommerce'
   },
-  {
-    id: 3,
-    title: 'Color Pallete App',
-    text: 'Using useState hooks, I was able to create the color change functionality. I used an npm package to handle the copy text function',
-    image: ColorPalleteApp,
-    tags: ['React', 'CSS'],
-    link: 'https://naughty-bhabha-1a8174.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-color-pallete'
-  },
-  {
-    id: 4,
-    title: 'Random Number Generator',
-    text: 'Using Javascript methods, I generated random numbers from the user input. It is a very simple project but it can also be useful to some.',
-    image: RandomNumberApp,
-    tags: ['React', 'CSS'],
-    link: 'https://boring-booth-4f6c67.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-random-number-generator'
-  },
 
-  {
-    id: 10,
-    title: 'We Swap App',
-    text: 'An idea from my brother, this project is to let people interact, swap items and negotiate.',
-    image: weswap,
-    tags: ['React', 'Under Development'],
-    link: 'https://stoic-neumann-99f11b.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-we-swap'
-  },
-  {
-    id: Date.now(),
-    title: 'Ilhwa Philippines Website',
-    text: 'A website for Ilhwa Company, a Korean Authenticated Ginseng Products, other herbal products, Vitamin, Supplement, Facial products etc.',
-    image: ilhwa,
-    tags: ['React', 'Static Website'],
-    link: 'https://modest-hypatia-36516e.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-ilhwa-website'
-  },
-  {
-    id: 6,
-    title: 'Expense Tracker',
-    text: 'A cash tracker that allows user manage their money using cash in, cashout system and also tracks expenses.',
-    image: expenseapp,
-    tags: ['HTML', 'CSS', 'Javascript', 'Bootstrap'],
-    link: 'https://philcobexpensecalculator.netlify.app/',
-    github: 'https://github.com/pjguitar15/expense-calculator'
-  },
-
-  {
-    id: 8,
-    title: 'Advice App',
-    text: 'A very simple Advice Web Application that generates a random advice from an API.',
-    image: adviceapp,
-    tags: ['React', 'CSS', 'Boostrap'],
-    link: 'https://modest-meninsky-058c79.netlify.app/',
-    github: 'https://github.com/pjguitar15/react-advice-app'
-  },
+  // {
+  //   id: 10,
+  //   title: 'We Swap App',
+  //   text: 'An idea from my brother, this project is to let people interact, swap items and negotiate.',
+  //   image: weswap,
+  //   tags: ['React', 'Under Development'],
+  //   link: 'https://stoic-neumann-99f11b.netlify.app/',
+  //   github: 'https://github.com/pjguitar15/react-we-swap'
+  // },
+  // {
+  //   id: Date.now(),
+  //   title: 'Ilhwa Philippines Website',
+  //   text: 'A website for Ilhwa Company, a Korean Authenticated Ginseng Products, other herbal products, Vitamin, Supplement, Facial products etc.',
+  //   image: ilhwa,
+  //   tags: ['React', 'Static Website'],
+  //   link: 'https://modest-hypatia-36516e.netlify.app/',
+  //   github: 'https://github.com/pjguitar15/react-ilhwa-website'
+  // },
 
 ]
 
