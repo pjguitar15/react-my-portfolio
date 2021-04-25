dcf56a1de93f1e8e311d25014e5f1256c148ce20
Phil <pjguitar15@gmail.com>
2021-04-25 19:09:09 +0800

    changed top right svg art size

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
           alt='bottomleft'
         />
         <img
-          style={{ position: 'absolute', top: '0', right: '0', width: '25rem' }}
+          style={{ position: 'absolute', top: '0', right: '0', width: '32rem' }}
           src={topright}
           alt='topright'
         />
